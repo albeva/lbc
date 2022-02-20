c82bf9c81150b8596b8e4de08df59c04ae99e846
Albert <albeva@me.com>
2022-02-20 15:54:55 +0000

    Update llvm build script

--- scripts/build-llvm.sh
+++ scripts/build-llvm.sh
@@ -1,16 +1,17 @@
-VERSION="llvm-12.0.0"
+VERSION="llvm-13.0.1"
 
-mkdir $VERSION-build
-cd $VERSION-build/
+mkdir $VERSION.build
+cd $VERSION.build/
 
-cmake -G "Unix Makefiles"				\
-	-DCMAKE_INSTALL_PREFIX=/usr/local	\
-	-DCMAKE_CXX_STANDARD=17				\
-	-DCMAKE_BUILD_TYPE=Release			\
-	-DLLVM_ENABLE_ASSERTIONS=ON			\
-	-DLLVM_TARGETS_TO_BUILD=X86			\
-	-DLLVM_OPTIMIZED_TABLEGEN=ON		\
-	-LLVM_INSTALL_UTILS=ON 				\
-	../$VERSION.src
+cmake -G "Unix Makefiles"           \
+  -DCMAKE_INSTALL_PREFIX=/usr/local \
+  -DCMAKE_CXX_STANDARD=17           \
+  -DCMAKE_BUILD_TYPE=Release        \
+  -DLLVM_ENABLE_ASSERTIONS=ON       \
+  -DLLVM_TARGETS_TO_BUILD=X86       \
+  -DLLVM_OPTIMIZED_TABLEGEN=ON      \
+  -DLLVM_INSTALL_UTILS=ON           \
+  ../$VERSION.src
 
 make -j12 install
+cd ..
