393501a5d6a8f5a3fbc7c01d72721eaa733443db
Albert <albeva@me.com>
2022-02-22 11:58:54 +0000

    Fix clang compile issue

--- src/Utils/Try.hpp
+++ src/Utils/Try.hpp
@@ -25,16 +25,17 @@ inline llvm::Error try_resolve_to_error(llvm::Error error) noexcept {
     return error;
 }
 
-#define TRY(expression)                              \
-    if (auto err = try_resolve_to_error(expression)) \
-        return err;
+#define TRY(expression)                                \
+    if (auto err = try_resolve_to_error(expression)) { \
+        return std::forward<llvm::Error>(err);         \
+    }
 
 #define TRY_ASSIGN(var, expression)                    \
     {                                                  \
         auto tryAssignValOrErr_ = (expression);        \
         if (auto err = tryAssignValOrErr_.takeError()) \
-            return err;                                \
-        var = *tryAssignValOrErr_;                     \
+            return std::move(err);                     \
+        (var) = *tryAssignValOrErr_;                   \
     }
 
 #define TRY_DECLARE(var, expression)      \
