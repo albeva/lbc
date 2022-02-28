79dd2c0ff7f4e12f2f414ecf858dea0c1b751453
Albert <albeva@me.com>
2022-02-28 00:58:15 +0000

    build llvm-14

--- scripts/build-llvm.sh
+++ scripts/build-llvm.sh
@@ -5,7 +5,7 @@ cd $VERSION.build/
 
 cmake -G "Unix Makefiles"           \
   -DCMAKE_INSTALL_PREFIX=/usr/local \
-  -DCMAKE_CXX_STANDARD=17           \
+  -DCMAKE_CXX_STANDARD=20           \
   -DCMAKE_BUILD_TYPE=Release        \
   -DLLVM_ENABLE_ASSERTIONS=ON       \
   -DLLVM_TARGETS_TO_BUILD=X86       \
