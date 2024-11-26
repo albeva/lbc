7a3dac023c418b7e29351575ccf112bb423cd6b6
Albert Varaksin <albeva@me.com>
2024-11-26 18:37:13 +0000

    Updated JIT

--- src/Driver/JIT.hpp
+++ src/Driver/JIT.hpp
@@ -12,7 +12,7 @@ public:
     : m_llJit(std::move(llJit)) {
     }
 
-    static llvm::Expected<std::unique_ptr<JIT>> create() {
+    static auto create() -> llvm::Expected<std::unique_ptr<JIT>> {
         auto llJit = llvm::orc::LLJITBuilder().create();
         if (!llJit) {
             return llJit.takeError();
@@ -21,30 +21,30 @@ public:
         return std::make_unique<JIT>(std::move(*llJit));
     }
 
-    [[nodiscard]] const llvm::DataLayout& getDataLayout() const {
+    [[nodiscard]] auto getDataLayout() const -> const llvm::DataLayout& {
         return m_llJit->getDataLayout();
     }
 
-    llvm::Error define(llvm::StringRef name, auto* addr, llvm::JITSymbolFlags flags) {
+    auto define(llvm::StringRef name, auto* addr, llvm::JITSymbolFlags flags) -> llvm::Error {
         llvm::orc::MangleAndInterner mangler{ m_llJit->getExecutionSession(), m_llJit->getDataLayout() };
 
         return m_llJit->getMainJITDylib().define(llvm::orc::absoluteSymbols(llvm::orc::SymbolMap{
             { mangler(name), llvm::orc::ExecutorSymbolDef(llvm::orc::ExecutorAddr::fromPtr(addr), flags) } }));
     }
 
-    llvm::Error addModule(llvm::orc::ThreadSafeModule module) {
+    auto addModule(llvm::orc::ThreadSafeModule module) -> llvm::Error {
         return m_llJit->addIRModule(std::move(module));
     }
 
-    llvm::Expected<llvm::orc::ExecutorAddr> lookup(llvm::StringRef name) {
+    auto lookup(llvm::StringRef name) -> llvm::Expected<llvm::orc::ExecutorAddr> {
         return m_llJit->lookup(name);
     }
 
-    llvm::Error initialize() noexcept {
+    auto initialize() noexcept -> llvm::Error {
         return m_llJit->initialize(m_llJit->getMainJITDylib());
     }
 
-    llvm::Error deinitialize() noexcept {
+    auto deinitialize() noexcept -> llvm::Error {
         return m_llJit->deinitialize(m_llJit->getMainJITDylib());
     }
 
