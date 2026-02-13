4685821d9e07b10dc7128eb19ab7d830460fb527
Albert Varaksin <albeva@me.com>
2026-02-13 20:52:28 +0000

    Updated cmake files

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,5 @@
 # Compiler options configuration
 add_library(compiler_options INTERFACE)
-
 if(MSVC AND NOT CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
     target_compile_options(compiler_options INTERFACE /Zc:__cplusplus /Zc:preprocessor)
 else()
